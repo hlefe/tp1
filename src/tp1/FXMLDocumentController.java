d02f3b8c4d2ad137e576a323350c01ea8d89ed49
Hugo Lefèvre <hugo.lefevre@gmail.com>
2015-11-20 11:54:15 +0100

    test

--- src/tp1/FXMLDocumentController.java
+++ src/tp1/FXMLDocumentController.java
@@ -8,6 +8,7 @@ package tp1;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -60,8 +61,14 @@ public class FXMLDocumentController implements Initializable {
         noteAnglais.setValueFactory(svalueAnglais);
         noteMaths.setValueFactory(svalueMaths);
         noteJava.setValueFactory(svalueJava);
+        quitter.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            System.exit(0);
+            }
+        });
        // int value = (Integer)noteAnglais.getValue();
-       moyenne.textProperty().bind(noteAnglais.editorProperty());
+       //moyenne.textProperty().bind(noteAnglais.editorProperty());
         //description.textProperty().bind(prénom.textProperty());
     }    
     
